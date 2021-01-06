7a69de1ec13594027fe0236bed36c27449bb6378
dillyg10 <dillyg10@gmail.com>
2021-01-06 17:25:54 -0500

    First commit

---
+++ src/main/java/com/blockempires/webstats/BlockEmpiresWebStatsApplication.java
@@ -0,0 +1,13 @@
+package com.blockempires.webstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockEmpiresWebStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockEmpiresWebStatsApplication.class, args);
+	}
+
+}
